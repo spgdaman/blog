c377afff05e67a2099dc5743009a827f0ee81dca
simon <gatheru.simon432@gmail.com>
2019-04-28 16:11:40 +0300

    random quote works and is rendered on random_quote page

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, abort, request, url_for, redirect
 from ..requests import get_random_quote
+from . import main
 
 @main.route('/random_quote')
 def show_quote():
     quote = get_random_quote()
-    return redirect('quotes.html',quotes=quote)
+    return render_template('quotes.html',quotes=quote)
