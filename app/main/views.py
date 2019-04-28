c0be0989f5b84962631e7fc3a10e987ce1a650b3
simon <gatheru.simon432@gmail.com>
2019-04-28 16:28:05 +0300

    index route defined and html updated, navbar styling added

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,10 @@ from flask import render_template, flash, abort, request, url_for, redirect
 from ..requests import get_random_quote
 from . import main
 
+@main.route('/')
+def index():
+    return render_template('index.html')
+
 @main.route('/random_quote')
 def show_quote():
     quote = get_random_quote()
