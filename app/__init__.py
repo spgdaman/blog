d28106730faedbff0f667fc95d9924ef51fe6b8a
simon <gatheru.simon432@gmail.com>
2019-04-28 15:25:20 +0300

    main blueprint registered in app factory

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,8 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     # initializing flask extensions
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
 
     return app
